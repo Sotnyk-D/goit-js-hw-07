b0ce56de9d6f443c9a079ceaa323476dd029f16b
Dmytro <112177564+Sotnyk-D@users.noreply.github.com>
2023-01-25 21:28:19 +0100

    add 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+"use strict";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector(".gallery");
+let preview = {};
+
+let galleryMarkup = createMarkup(galleryItems);
+imgContainer.innerHTML = galleryMarkup;
+
+imgContainer.addEventListener("click", onClickImg);
+function createMarkup(galleryData) {
+  return galleryData
+    .map(({ preview, original, description }) => {
+      return `
+         <div class="gallery__item">
+         <a class=@gallery__link"href="${original}">
+         <img class="gallery__img" src="${preview}"
+         data-source="${original}"
+          alt="${description}"/>
+          </a>
+          </div>
+          `;
+    })
+    .join("");
+}
+
+function onClickImg(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  createBasicLightbox(event.target);
+  modalOpen(preview);
+}
+
+function createBasicLightbox(img) {
+  preview = basicLightbox.create(
+    `<img src="${img.getAttribute("data-sourse")}" width="800" height="600">`
+  );
+}
+
+function modalOpen() {
+  preview.show();
+  window.addEventListener("keydown", closeOnEscPress);
+}
+
+function closeOnEscPress(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  preview.close();
+  window.removeEventListener("keydown", closeOnEscPress);
+}
